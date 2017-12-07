c7fd085c5671944f492bdf3e9c8656d8f725f583
ITCSBarbie <itcsbarbie@gmail.com>
2017-12-07 14:58:08 -0600

    added Procfile

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,33 @@ var express = require('express');
 var router = express.Router();
 var exchangeRates = require('../model/currencyDB');
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', author: "Karena" });
+    res.render('index', { title : 'Express' });
 });
-router.get('/about', function(req, res, next) {
-    res.render('about', { name: 'awesome site!' });
-    });
+
 /* GET convert page */
 router.get('/convert', function(req, res, next){
 
+    var dollars = req.query.dollars;   // How many dollars?
+    var toCurrency = req.query.to_currency;  // To what currency?
 
-    var query = req.query;
+    var converted = dollars * exchangeRates[toCurrency];   // math!
+
+    res.render('results', {
+        dollars: dollars,
+        toCurrency: toCurrency,
+        converted: converted}
+    );
 
 });
+
+/* GET about page */
+router.get('/about', function(req, res, next){
+    res.render('about', { name: "My awesome currency converter site"});
+});
+
+
 
 module.exports = router;
